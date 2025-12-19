c3d2cc8073954faa936aa68b4017a319246839a2
YulinWangFAU <yulinwang_biopic@sina.com>
2025-12-19 12:07:45 +0100

    modify AutoRG_Brain/run/load_pretrained_weights.py weights_only=False

--- AutoRG_Brain/run/load_pretrained_weights.py
+++ AutoRG_Brain/run/load_pretrained_weights.py
@@ -18,7 +18,7 @@ def load_pretrained_weights_allow_missing(network, fname, verbose=False):
     """
     THIS DOES NOT TRANSFER SEGMENTATION HEADS!
     """
-    saved_model = torch.load(fname)
+    saved_model = torch.load(fname, map_location='cpu', weights_only=False)
     pretrained_dict = saved_model['state_dict']
 
     new_state_dict = {}
@@ -72,7 +72,7 @@ def load_pretrained_weights(network, fname, verbose=False):
     """
     THIS DOES NOT TRANSFER SEGMENTATION HEADS!
     """
-    saved_model = torch.load(fname)
+    saved_model = torch.load(fname, map_location='cpu', weights_only=False)
     pretrained_dict = saved_model['state_dict']
 
     new_state_dict = {}
